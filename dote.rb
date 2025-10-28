be8446a16c891730f86820d9e0d178542a5caa39
hasita-ryu <kmc2533@kamiyama.ac.jp>
2025-10-28 11:34:42 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -1,13 +1,15 @@
 class Dote < Formula
   desc "dot e command"
-  homepage "https://github.com/kmc2533/homebrew-dote"
-  url "https://raw.githubusercontent.com/kmc2533/homebrew-dote/main/dote.c"
+  homepage "https://github.com/hasita-ryu/homebrew-dote"
+  url "https://raw.githubusercontent.com/hasita-ryu/homebrew-dote/main/dot3.c"
   sha256 "b1d9fd24048aa23154e4dafcb706a7dbd2d6cf6935c723ef39cae60e62a3223c"
   version "1.0.0"
-  
+
   def install
-    system ENV.cc, "dote.c", "-o", "dote"
+   
+    system ENV.cc, "dot3.c", "-o", "dote"
+
+    
     bin.install "dote"
   end
 end
-
